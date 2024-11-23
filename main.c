437e24c6c54fe627ecc7b2a1b5d0aec6c195908f
Yamil <invitado@MacBook-Pro-de-Invitado.local>
2024-11-22 18:43:13 -0600

    Add make reserve finction with only input logic

--- main.c
+++ main.c
@@ -14,8 +14,10 @@ struct reserve {
   struct date date_out;
   int room_number;
   bool continental;
-  };
+  float cost;
+};
 
+int make_reserve(/* char[], int, char[], char[], int, bool */);
 
 int main() {
   struct reserve reserves[25];
@@ -33,7 +35,31 @@ int main() {
     scanf("%d", &response);
 
     if (response == 6) exit = true;
+    if (response == 1) make_reserve();
   } while(!exit);
 
+  return 0;
+}
+
+int make_reserve() {
+  struct reserve new_reserve;
+  int continental_temp;
+
+  printf("Nombre del cliente:\n");
+  scanf("%s", new_reserve.name);
+  printf("Codigo de reserva:\n");
+  scanf("%d", &new_reserve.id);
+  printf("Numero de habitacion:\n");
+  scanf("%d", &new_reserve.room_number);
+  printf("Incluye desayuno (y/n):\n");
+  scanf("%d", &continental_temp);
+  // Assign boolean value to continental in struct
+  if (continental_temp == true) {
+    new_reserve.continental = true;
+  } else {
+    new_reserve.continental = false;
+  }
+
+  printf("%d\n", new_reserve.continental);
   return 0;
 }
